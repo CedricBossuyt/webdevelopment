e79af8dee87b59adf2dd864d55bd1d2a59ab54a6
Cédric Bossuyt <cedric.bossuyt2@student.vives.be>
2023-04-17 11:05:45 +0200

    Labo 20

--- labo_20/Memory_Card/scripts/code.js
+++ labo_20/Memory_Card/scripts/code.js
@@ -2,10 +2,23 @@ let global = {
     AANTAL_HORIZONTAAL: 4,
     AANTAL_VERTICAAL: 3,
     AANTAL_KAARTEN: 6,
+    AANTAL_KAARTEN_OPTAFEL: 12,
     kaartLijst: []
 };
 const setup = () => {
+    let kaartTafel = document.createElement("div")
+    kaartTafel.classList.add("kaartTafel");
+    document.body.appendChild(kaartTafel)
 
+    for (let i = 0; i < global.AANTAL_KAARTEN_OPTAFEL; i++) {
+        let kaart = document.createElement("img")
+        kaart.className = "achterkant"
+        kaart.id = "kaart" + i;
+        kaart.src = "images/achterkant.jpg"
+        kaart.onclick = klik;
+        kaartTafel.appendChild(kaart);
+        global.kaartLijst.push(kaart);
+    }
 }
 const klik = () => {
     
