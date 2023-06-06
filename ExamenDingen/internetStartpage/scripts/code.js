e54294c566dbcd97d06acfad22035053070ba503
Cédric Bossuyt <cedric.bossuyt2@student.vives.be>
2023-06-06 14:58:05 +0200

    fixke

--- ExamenDingen/internetStartpage/scripts/code.js
+++ ExamenDingen/internetStartpage/scripts/code.js
@@ -7,28 +7,20 @@ const setup = () => {
     if(objectlinkskes !== null){
         if(objectlinkskes.linkLijst !== null){
             global.linkLijst = objectlinkskes.linkLijst
-            for (let i = 0; i < global.linkLijst; i++) {
+            for (let i = 0; i < global.linkLijst.length; i++) {
                 let history = document.getElementById('history')
                 let card = document.createElement('div')
                 card.className = 'cardLink'
-                let titel = document.createElement('p')
-                let beschrijving = document.createElement('p')
                 if (global.linkLijst[i].includes('google')){
                     card.style.backgroundColor = 'rgb(0,0,255)'
                 }else if(global.linkLijst[i].includes('youtube')){
-                    let textTitel = document.createTextNode("Youtube")
-                    let textBeschrijving = document.createTextNode(yZoekOpdracht)
-                    titel.appendChild(textTitel)
-                    beschrijving.appendChild(textBeschrijving)
                     card.style.backgroundColor = 'rgb(255,0,0)'
                 }
                 let smallGoButton = document.createElement('input')
                 smallGoButton.type = 'button'
                 smallGoButton.value = 'go!'
                 smallGoButton.className = 'smallGoButton'
-                smallGoButton.addEventListener("click", () => { open(link) })
-                card.appendChild(titel)
-                card.appendChild(beschrijving)
+                smallGoButton.addEventListener("click", () => { open(global.linkLijst[i]) })
                 card.appendChild(smallGoButton)
                 history.appendChild(card)
             }
@@ -46,7 +38,7 @@ const goButtonKlik = () => {
     }else if(input.value.startsWith('/y')) {
         let yZoekOpdracht = input.value.replace('/y', '')
         open('https://www.youtube.com/results?search_query=' + yZoekOpdracht)
-        let link = 'https://www.youtube.com/results?search_query=' + yZoekOpdracht
+        let link = 'https://www.youtube.com/results?search_query='+yZoekOpdracht
         global.linkLijst.push(link)
         makeCard(link)
     }else if(input.value.startsWith('/')){
@@ -54,8 +46,7 @@ const goButtonKlik = () => {
     }else{
         window.alert('die kommando ken ik tog niet mwoan')
     }
-
-    localStorage.setItem("linkjes", JSON.stringify(global.linkLijst))
+    localStorage.setItem("linkjes", JSON.stringify(global))
 }
 const makeCard = (link) =>{
     let input = document.getElementById('zoekOpdracht')
